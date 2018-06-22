5a36d5e923862e2d57a3353064d4b0810946afb9
Peter DeGroff <peter.degroff@gmail.com>
2018-06-22 17:28:31 +1000

    add shonky agency search

--- board/src/pages/messages/compose.js
+++ board/src/pages/messages/compose.js
@@ -1,7 +1,60 @@
 import React, { Component, Fragment } from 'react'
 import markdownify from 'draftjs-to-markdown'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+import styled from 'styled-components'
 
 import Editor from '../../components/editor'
+import { Text } from '../../components/forms'
+
+const queryServices = gql`
+  query {
+    services {
+      id
+      name
+      description
+      contactNo
+      agency {
+        name
+      }
+    }
+  }
+`
+
+const Wrapper = styled.section``
+
+const Target = styled.div``
+
+const Services = styled.ul`
+  display: none;
+  list-style: none;
+  padding: 0;
+  border: 1px solid #eee;
+`
+
+const Search = styled(Text)`
+  &:focus + ${Services} {
+    display: block;
+  }
+`
+
+const Service = styled.li`
+  font-weight: ${props => (props.active ? 'bold' : 'normal')};
+  padding: 1rem 2rem;
+
+  & + & {
+    margin-top: 0;
+    border-top: 1px solid #eee;
+  }
+`
+
+const ServiceName = styled.div``
+
+const ServiceDescription = styled.div`
+  margin-top: 0;
+  opacity: 0.7;
+  font-size: 0.8em;
+`
 
 class Compose extends Component {
   state = { editorContent: '' }
@@ -15,13 +68,42 @@ class Compose extends Component {
 
   render() {
     return (
-      <Fragment>
+      <Wrapper>
+        <Target>
+          <span>To:</span>
+          <Query query={queryServices}>
+            {({ loading, error, data }) =>
+              loading ? (
+                <div>loading...</div>
+              ) : (
+                <Fragment>
+                  <Search placeholder="agency or service" />
+                  <Services>
+                    {(data.services || []).map((service, i) => (
+                      <Service>
+                        <ServiceName>
+                          {service.name} @ {service.agency.name}
+                        </ServiceName>
+                        {service.description && (
+                          <ServiceDescription>
+                            {service.description}
+                          </ServiceDescription>
+                        )}
+                      </Service>
+                    ))}
+                  </Services>
+                </Fragment>
+              )
+            }
+          </Query>
+        </Target>
+
         <Editor
           onContentStateChange={contentState => {
             this.setEditorContent(markdownify(contentState))
           }}
         />
-      </Fragment>
+      </Wrapper>
     )
   }
 }
