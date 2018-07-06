8360cdb452b1ac6513b5b753454ea18cdf613f71
Peter DeGroff <peter.degroff@digital.gov.au>
2018-07-06 11:26:30 +1000

    cleanup(components): remove old styles, components

--- board/src/pages/messages/components/index.js
+++ board/src/pages/messages/components/index.js
@@ -91,24 +91,6 @@ export const Message = styled.li`
   }
 `
 
-export const ReadMessage = styled(Message)`
-  background-color: #f3f5f5;
-`
-
-export const Sender = styled.span`
-  font-weight: ${props => (props.status === 'Unread' ? 'bold' : 'normal')};
-`
-
-export const Subject = styled.span`
-  font-weight: ${props => (props.status === 'Unread' ? 'bold' : 'normal')};
-
-  & + ${Sender} {
-    border-left: 1px solid ${props => props.theme.copyColour};
-    margin-left: 1rem;
-    padding-left: 1rem;
-  }
-`
-
 export const ShortSubject = styled.span`
   font-weight: ${props => (props.unread ? 'bold' : 'normal')};
   white-space: nowrap;
@@ -121,59 +103,6 @@ export const ShortSender = styled.span`
   white-space: nowrap;
   text-overflow: ellipsis;
   font-weight: ${props => (props.unread ? 'bold' : 'normal')};
-`
-
-export const About = styled.header`
-  a {
-    color: ${props => props.theme.copyColour};
-    text-decoration: none;
-  }
-`
-
-const lozengeColour = ({ overdue, important, information }) => {
-  if (information) {
-    return css`
-      padding: 0;
-      border-radius: 0;
-    `
-  }
-
-  if (true) {
-    return css`
-      background-color: transparent;
-      border: 1px solid #bbb;
-    `
-  }
-
-  if (overdue) {
-    return css`
-      background-color: #f4d0d6;
-      color: #c42d37;
-    `
-  }
-
-  if (important) {
-    return css`
-      background-color: #f8dabe;
-      color: #ac5d24;
-    `
-  }
-
-  return css`
-    background-color: #dcdcdc;
-  `
-}
-
-export const Lozenge = styled(
-  ({ overdue, important, information, className, ...props }) => (
-    <span className={classnames('lozenge', className)} {...props} />
-  )
-)`
-  font-size: 0.8em;
-  display: inline-block;
-  border-radius: 1em;
-  padding: 0.2rem 1.5rem;
-  ${lozengeColour};
 `
 
 export const Document = styled(({ className, ...props }) => (
@@ -211,51 +140,4 @@ export const Document = styled(({ className, ...props }) => (
       }
     }
   }
-`
-
-export const Features = styled.div`
-  margin-top: 0;
-  display: flex;
-  flex-flow: column nowrap;
-
-  ${props =>
-    props.lozenges
-      ? css`
-          margin-left: -1.5rem;
-        `
-      : css``}
-  }
-
-  ${Lozenge} {
-    margin-top: 1rem;
-  }
-
-  ${Document} {
-    margin-top: 1em;
-  }
-
-
-  @media screen and (min-width: 768px) {
-    flex-flow: row wrap;
-
-    ${Document}, ${Lozenge} {
-      margin-right: 1em;
-    }
-  }
-`
-
-export const Prompt = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  margin: 0 1rem;
-
-  * + * {
-    margin-top: 0;
-  }
-`
-
-export const Attachment = styled.div``
-
-export const Timestamp = styled.div`
-  font-size: 0.8em;
 `
